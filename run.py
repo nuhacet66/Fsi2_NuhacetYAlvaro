f474e84aa08106d2d13d6d5abdf4b68745447f0f
Nuhacet Travieso del Rosario <nuhacet66@gmail.com>
2024-12-08 18:40:50 +0000

    Create the AI and training with the default data

---
+++ run.py
@@ -0,0 +1,200 @@
+
+import tensorflow
+tensorflow.test.gpu_device_name()
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
+
+#matplotlib inline
+pil_im = Image.open('/kaggle/input/sports-database/train/air hockey/001.jpg', 'r')
+
+
+imshow(np.asarray(pil_im))
+
+from tensorflow.keras import backend as K
+from time import time
+
+# DATA SOURCE --------------------------------------------------
+
+train_data_dir = '/kaggle/input/sports-database/train'
+validation_data_dir = '/kaggle/input/sports-database/valid'
+test_data_dir = '/kaggle/input/sports-database/test'
+image_size = (224, 224)
+batch_size = 32
+
+train_ds = tensorflow.keras.preprocessing.image_dataset_from_directory(
+    train_data_dir,
+    #validation_split=0.2,
+    #subset="training",
+    #seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+    label_mode='categorical'
+)
+
+validation_ds = tensorflow.keras.preprocessing.image_dataset_from_directory(
+    validation_data_dir,
+    #validation_split=0.2,
+    #subset="validation",
+    #seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+    label_mode='categorical'
+)
+
+test_ds = tensorflow.keras.preprocessing.image_dataset_from_directory(
+    test_data_dir,
+    #validation_split=0.2,
+    #subset="validation",
+    #seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+    label_mode='categorical'
+)
+
+train_ds = train_ds.prefetch(buffer_size=32)
+validation_ds = validation_ds.prefetch(buffer_size=32)
+test_ds = test_ds.prefetch(buffer_size=32)
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Rescaling                    
+
+# MODEL --------------------------------------------------
+
+model = Sequential()
+model.add(Rescaling(scale=(1./127.5),offset=-1, input_shape=(224, 224, 3)))
+
+#model.add(Conv2D(16, kernel_size=(3, 3), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(12, activation='softmax'))
+
+model.summary()
+
+model.compile(loss=tensorflow.keras.losses.categorical_crossentropy,
+              optimizer=tensorflow.keras.optimizers.Adam(1e-3),
+              metrics=['accuracy'])
+
+# TRAINING --------------------------------------------------
+from tensorflow.keras.callbacks import EarlyStopping
+
+epochs = 200
+
+es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=10, restore_best_weights=True)
+
+history = model.fit(
+          train_ds,
+          epochs=epochs,
+          validation_data = validation_ds,
+          callbacks = [es]
+)
+
+# SAVING --------------------------------------------------
+
+model.save("mimodelo.h5")
+
+# Evaluate the model on the test data using `evaluate`
+
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sns
+
+results = np.concatenate([(y, model.predict(x=x)) for x, y in validation_ds], axis=1)
+
+labels = np.argmax(results[0], axis=1)
+predictions = np.argmax(results[1], axis=1)
+
+cf_matrix = confusion_matrix(labels, predictions)
+
+sns.heatmap(cf_matrix, annot=True, fmt="d", cmap="Blues")
+
+#disp = ConfusionMatrixDisplay(confusion_matrix=cf_matrix)
+#disp.plot()
+
+print(classification_report(labels, predictions, digits = 4))
+from matplotlib import pyplot as plt 
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='validation accuracy')
+plt.plot(history.history['loss'], label='loss')
+
+plt.title('Entrenamiento Deportes')
+
+plt.xlabel('Épocas')
+plt.ylabel('Exactitud')
+plt.legend(loc="upper right")
+
+plt.show()
+
+# PRODUCTION ----------------------------------------------
+
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
+import tensorflow.keras
+
+# LOADING --------------------------------------------------
+#model = tensorflow.keras.models.load_model("mimodelo.h5")
+
+etiquetas=['Air Hockey', 'Baseball', 'Basketball', 'Football', 'F1 Racing', 'Golf', 'Hockey','Judo', 'Motorcycle Racing', 'Nascar Racing',  'Rugby', 'Tennis']
+#matplotlib inline
+
+pil_im = Image.open('/kaggle/input/sports-database/train/formula 1 racing/002.jpg', 'r')
+im = np.asarray(pil_im.resize((224, 224)))
+imshow(im)
+print(im.shape) # La imagen es un array de dimensión: 224x224x3
+
+# El método `predict` hace la predicción de un lote de entradas, no solo una. 
+# En el caso de que tengamos solo una entrada deberemos añadirle una dimensión más 
+# al array numpy para que la entrada tenga la dimensión: 1x224x224x3
+
+im = im.reshape(1,224,224,3)
+
+
+print('El vector de salida obtenido: ', model.predict(im))
+print('La etiqueta de salida predicha es ', np.argmax(model.predict(im)))
+print('Ahora dicho con texto: La etiqueta de salida predicha es ', etiquetas[np.argmax(model.predict(im))])
+
+
+
+# PRODUCTION ----------------------------------------------
+
+#from matplotlib.pyplot import imshow
+import numpy as np
+#from PIL import Image
+import tensorflow.keras
+
+# SAVING --------------------------------------------------
+#model.save_model("mimodelo.h5")
+
+
+# LOADING --------------------------------------------------
+#model = tensorflow.keras.models.load_model("mimodelo.h5")
+
+
+etiquetas=['Air Hockey', 'Baseball', 'Basketball', 'Football', 'F1 Racing', 'Golf', 'Hockey','Judo', 'Motorcycle Racing', 'Nascar Racing',  'Rugby', 'Tennis']
+#matplotlib inline
+print("ETIQUETA PREDICHA -> ETIQUETA REAL")
+for minilote in test_ds:
+    prediccion_minilote = model.predict(minilote[0].numpy())
+    etiqueta_real_minilote = minilote[1].numpy()
+    for y_predicha, y_real in zip(np.round(prediccion_minilote,3), etiqueta_real_minilote):
+        if np.argmax(y_predicha) == np.argmax(y_real):
+            print(etiquetas[np.argmax(y_predicha)], "->", etiquetas[np.argmax(y_real)])
+        else:
+            print(etiquetas[np.argmax(y_predicha)], "->", etiquetas[np.argmax(y_real)], "✘")
